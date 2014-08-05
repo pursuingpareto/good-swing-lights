e2a5a7b4826958f6baf3763b659a1638db459934
pursuingpareto <pursuingpareto@gmail.com>
2014-08-05 13:56:36 -0700

    respect on/off state of LEDs

--- HTMLActuator.js
+++ HTMLActuator.js
@@ -1,10 +1,10 @@
-var makeLight = function(containerElement, i) {
+var makeLED = function(containerElement, i) {
 	var newLight = document.createElement("div");
-	newLight.className += "light";
-	newLight.setAttribute('id','light-'+String(i));
+	newLight.className += "LED";
+	newLight.setAttribute('id','LED-'+String(i));
 	containerElement.appendChild(newLight);;
 }
-var makeLights = function(LightStrip) {
+var makeLEDs = function(LightStrip) {
 	var poles = document.getElementsByClassName('pole');
 	console.log(poles.length);
 	var numLEDs = LightStrip.allLeds.length;
@@ -13,7 +13,7 @@ var makeLights = function(LightStrip) {
 		var pole = poles[poleIndex];
 		console.log(pole);
 		for (var i=0; i<numLEDs; i++) {
-			makeLight(pole, i);
+			makeLED(pole, i);
 		}
 	}
 }
@@ -21,13 +21,21 @@ var HTMLActuator = function(LightStrip){
 	var thisActuator = {
 		actuate : function() {
 			console.log('actuating...');
-			makeLights(LightStrip);
+			makeLEDs(LightStrip);
 		},
 		update : function() {
-			var lights = document.getElementsByClassName('light');
+			var lights = document.getElementsByClassName('LED');
 			for (var i=0; i<lights.length; i++) {
-				var lightElement = document.getElementById('light-'+String(i))
+				var lightElement = document.getElementById('LED-'+String(i))
 				var led = LightStrip.allLeds[i];
+				// hacky toggler
+				if (led.state === 'off') {
+					lightElement.classList.toggle('on', false);
+					lightElement.classList.toggle('off', true)
+				} else {
+					lightElement.classList.toggle('on', true)
+					lightElement.classList.toggle('off', false);
+				}
 				// console.log('updating led number:');
 				// console.log(i);
 				// console.log('with rgbColor:');
